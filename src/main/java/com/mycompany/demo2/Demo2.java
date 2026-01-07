def3d5690410d72078112d1a4f4a74528a49936f
fotzs_41 <thanhdanh41206@gmail.com>
2026-01-07 12:30:58 +0700

    Upload Tuan

---
+++ src/main/java/com/mycompany/demo2/Demo2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo2;
+
+/**
+ *
+ * @author FOTZS41
+ */
+public class Demo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        /// dmmm tuan
+    }
+}
