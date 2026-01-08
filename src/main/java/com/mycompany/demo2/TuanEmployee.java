6c662c74539a4dce7e9b690e8862a80674a6d67e
fotzs_41 <thanhdanh41206@gmail.com>
2026-01-08 07:47:27 +0700

    Tuan Employee

---
+++ src/main/java/com/mycompany/demo2/TuanEmployee.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.demo2;
+
+/**
+ *
+ * @author FOTZS41
+ */
+public class TuanEmployee {
+
+}
